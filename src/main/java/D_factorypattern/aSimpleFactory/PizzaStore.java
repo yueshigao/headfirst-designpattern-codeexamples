6bc03b049fa961264db8e3f3f9bd650afe4e094f
YueJessicaGAO <yueshi.gao@gmail.com>
2019-02-14 11:52:37 +0100

    Strategy Pattern 3: = (Simple) Factory Pattern

--- src/main/java/D_factorypattern/aSimpleFactory/PizzaStore.java
+++ src/main/java/D_factorypattern/aSimpleFactory/PizzaStore.java
@@ -9,12 +9,16 @@ public class PizzaStore {
 
     SimplePizzaFactory factory;
 
+    /**exactly the same as Strategy Pattern
+     * 1.Injection Dependence in constructor
+     * 2. delegate the create action to this dependency **/
     public PizzaStore(SimplePizzaFactory factory) {
         this.factory = factory;
     }
 
     public Pizza orderPizza(String type) {
 
+         /** 2. delegate the create action to this dependency **/
         Pizza pizza = factory.createPizza(type);
 
         pizza.prepare();
