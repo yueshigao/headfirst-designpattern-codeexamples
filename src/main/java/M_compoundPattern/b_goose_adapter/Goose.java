70d033227388a6bdb749a85d711767545de851bc
YueJessicaGAO <yueshi.gao@gmail.com>
2019-02-15 14:49:09 +0100

    Compound Pattern 2: adapter pattern - adapter(goose adapter) implements target(duck), real object(goose) is injected, and target action(quack) is delegated to the real object(honk)

---
+++ src/main/java/M_compoundPattern/b_goose_adapter/Goose.java
@@ -0,0 +1,7 @@
+package M_compoundPattern.b_goose_adapter;
+
+public class Goose {
+    void honk() {
+        System.out.println("Honk");
+    }
+}
---
+++ src/main/java/M_compoundPattern/b_goose_adapter/GooseAdapter.java
@@ -0,0 +1,17 @@
+package M_compoundPattern.b_goose_adapter;
+
+import M_compoundPattern.a_quackable_strategy.Quackable;
+
+public class GooseAdapter implements Quackable {
+
+    private Goose goose;
+
+    public GooseAdapter(Goose goose) {
+        this.goose = goose;
+    }
+
+    @Override
+    public void quack() {
+        goose.honk();
+    }
+}
